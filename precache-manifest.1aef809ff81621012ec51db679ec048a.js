be754dfdc2634757417d9089a3bf4229a41477ff
Vita <vitalia@myrambler.ru>
2019-09-09 21:20:41 +0300

    Updates

---
+++ precache-manifest.1aef809ff81621012ec51db679ec048a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34469f4c5bf992fefa3dccdd86ee8c42",
+    "url": "/poetry-site/index.html"
+  },
+  {
+    "revision": "89beaefea2a40fd1f59c",
+    "url": "/poetry-site/static/css/main.0d82f4aa.chunk.css"
+  },
+  {
+    "revision": "07d72a2f2ff446aa9dbb",
+    "url": "/poetry-site/static/js/2.15148fd3.chunk.js"
+  },
+  {
+    "revision": "89beaefea2a40fd1f59c",
+    "url": "/poetry-site/static/js/main.e0a2201c.chunk.js"
+  },
+  {
+    "revision": "c3192e2fab15f98f2113",
+    "url": "/poetry-site/static/js/runtime~main.9d8ae27c.js"
+  },
+  {
+    "revision": "f28e284ccc9e2d57e8cb0a332a7800cd",
+    "url": "/poetry-site/static/media/bg.f28e284c.jpg"
+  }
+]);
